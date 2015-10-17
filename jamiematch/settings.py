fc31413c943aa24b089cf5e3a101610864431cd4
Shun Liang <shun.liang.public@gmail.com>
2015-10-17 17:40:45 +0100

    Added dummy static file.

--- jamiematch/settings.py
+++ jamiematch/settings.py
@@ -131,3 +131,7 @@ STATICFILES_DIRS = (
             os.path.join(BASE_DIR, 'static'),
 )
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
