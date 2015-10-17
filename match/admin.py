a3d753bac6f1bbac338d6415d2fa1c9aa443c21a
Shun Liang <shun.liang.public@gmail.com>
2015-10-17 17:57:20 +0100

    Register admins.

--- match/admin.py
+++ match/admin.py
@@ -5,5 +5,9 @@ from .models import Customer, Chef
 class CustomerAdmin(admin.ModelAdmin):
    model = Customer 
 
+admin.site.register(Customer, CustomerAdmin)
+
 class ChefAdmin(admin.ModelAdmin):
    model = Chef
+
+admin.site.register(Chef, ChefAdmin)
