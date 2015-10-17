b6da309fe0aa5c8e87f0c3232749ba7781fface6
Shun Liang <shun.liang.public@gmail.com>
2015-10-17 18:06:56 +0100

    Added __str__ method to models.

--- match/models.py
+++ match/models.py
@@ -4,9 +4,13 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(User)
     city = models.TextField()
+    def __str__(self):
+        return "User ID: %s; City: %s", user.username, city
 
 class Chef(models.Model):
     user = models.OneToOneField(User)
     address = models.TextField()
     city = models.TextField()
     post_code = models.TextField()
+    def __str__(self):
+        return "User ID: %s; Address: %s; City: %s, post_code: %s", user.username, address, city, post_code
